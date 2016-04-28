22eaf73d2b53cd5f544da51f3b0340d14d40d6f2
Coda Phillips <codaphillips@gmail.com>
2016-04-28 06:21:01 +0000

    Try to make category page

---
+++ category-spring-2016.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: Journal Article
+ *
+ * @package Edin
+ */
+
+get_header(); ?>
+
+	<div class="hero">
+		<div class="hero-wrapper">
+			<div>
+			<h1 class="page-title">
+				<?php single_cat_title(); ?>
+			</h1>
+			</div>
+			<div>
+			<img width=300px src="/wp-content/uploads/2016/04/cover.png"></img>
+			</div>
+		</div>
+	</div>
+
+	<div class="content-wrapper clear">
+
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
+				<ul class="article-links">
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<li>
+						<a href="<?php echo get_permalink() ?>"><h3><?php the_title() ?></h3></a>
+						<em>
+						<?php
+							$author = get_post_meta(get_the_ID(), 'author', true);
+							echo $author;
+						?>
+						</em>
+					</li>
+
+				<?php endwhile; // end of the loop. ?>
+				</ul>
+
+			</main><!-- #main -->
+		</div><!-- #primary -->
+
+	</div><!-- .content-wrapper -->
+
+<?php get_footer(); ?>
