dbf0f8aec54b9d81540e5abc6bdf2436eb1034a2
Coda Phillips <codaphillips@gmail.com>
2016-04-28 05:20:09 +0000

    Make category page

--- category.php
+++ category.php
@@ -1,66 +1,43 @@
 <?php
 /**
- * The template for displaying Category pages.
- *
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ * Template Name: Journal Article
  *
  * @package Edin
  */
 
 get_header(); ?>
 
-	<div class="hero <?php echo edin_additional_class(); ?>">
-		<?php if ( have_posts() ) : ?>
-
-			<div class="hero-wrapper">
-				<h1 class="page-title">
-					<?php
-						single_cat_title();
-					?>
-				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</div>
-
-		<?php else : ?>
-
-			<div class="title-wrapper">
-				<h1 class="page-title"><?php _e( 'Nothing Found', 'edin' ); ?></h1>
-			</div>
-
-		<?php endif; ?>
-	</div><!-- .hero -->
+	<div class="hero">
+		<div class="hero-wrapper">
+			<h1 class="page-title">
+				<?php single_cat_title(); ?>
+			</h1>
+		</div>
+	</div>
 
 	<div class="content-wrapper clear">
 
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
+				<ul class="article-links">
+				<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php if ( have_posts() ) : ?>
+					<li>
+						<a href="<?php echo get_permalink() ?>"><h3><?php the_title() ?></h3></a>
+						<em>
+						<?php
+							$author = get_post_meta(get_the_ID(), 'author', true);
+							echo $author;
+						?>
+						</em>
+					</li>
 
-					<?php
-						/* Include the Post-Format-specific template for the content.
-						 * If you want to override this in a child theme, then include a file
-						 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-						 */
-						// The Query
-						the_category_wp_tiles();
-					?>
-					<?php edin_paging_nav(); ?>
-
-				<?php else : ?>
-
-					<?php get_template_part( 'content', 'none' ); ?>
-
-				<?php endif; ?>
+				<?php endwhile; // end of the loop. ?>
+				</ul>
 
 			</main><!-- #main -->
 		</div><!-- #primary -->
 
-<?php get_sidebar(); ?>
+	</div><!-- .content-wrapper -->
+
 <?php get_footer(); ?>
